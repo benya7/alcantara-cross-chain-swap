3ab11f0987821f1c685ec54cacbd9089bf709392
Samsung <bitup.games@gmail.com>
2022-12-14 20:23:36 +0100

    add TkDetails component

---
+++ components/TxDetails.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSwap } from '../contexts/Swap'
+import { TbGasStation } from "react-icons/tb";
+import Spinner from './Layout/Spinner';
+export default function TxDetails() {
+  const { fullGasCostInUsd } = useSwap()
+  return (
+    <div className='flex justify-between px-2 items-center'>
+      <p className='text-sm inline-flex items-center'><TbGasStation className='mr-1' /> Tx cost:</p>
+      {
+        fullGasCostInUsd.state === 'done' ? (
+          <p className='text-sm'>~${fullGasCostInUsd.value.toFixed(2)}</p>
+          ) : (
+            <div className='bg-slate-500 rounded animate-pulse w-1/4 h-5'></div>
+          )
+      } 
+    </div>
+  )
+}
