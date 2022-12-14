76b7a67b63e511b0ae4bf8606ea8c93937a593b6
Samsung <bitup.games@gmail.com>
2022-12-14 20:38:50 +0100

    update unused import

--- wagmi.ts
+++ wagmi.ts
@@ -4,30 +4,25 @@ import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import localChains from "./config/localChains.json";
 import { publicProvider } from "wagmi/providers/public";
 import { chainsConfigData } from "./config/chains";
 
-const parsedLocalChains: Chain[] = localChains.map((localChain) => ({
-  id: localChain.chainId,
-  name: localChain.name,
-  network: `local-${localChain.chainId}`,
-  nativeCurrency: {
-    name: localChain.tokenName,
-    symbol: localChain.tokenSymbol,
-    decimals: 18
-  },
-  rpcUrls: {
-    default: localChain.rpc
-  }
-}));
+// const parsedLocalChains: Chain[] = localChains.map((localChain) => ({
+//   id: localChain.chainId,
+//   name: localChain.name,
+//   network: `local-${localChain.chainId}`,
+//   nativeCurrency: {
+//     name: localChain.tokenName,
+//     symbol: localChain.tokenSymbol,
+//     decimals: 18
+//   },
+//   rpcUrls: {
+//     default: localChain.rpc
+//   }
+// }));
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [
-    ...(process.env.NODE_ENV !== "development"
-      ? [...parsedLocalChains]
-      : [chain.mainnet, chain.polygon, chain.arbitrum, ...chainsConfigData]),
-  ],
+  [chain.mainnet, chain.polygon, chain.arbitrum, ...chainsConfigData],
   [publicProvider()]
 );
 
