0f46cae71cf37b8392be8fa87786fa66f6043801
Samsung <bitup.games@gmail.com>
2022-12-14 20:48:12 +0100

    add launch dapp button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className='min-h-screen bg-black text-slate-100'>
-      <div className="flex items-center h-[26rem] bg-gradient-to-b from-slate-900 to-black">
+      <div className="flex flex-col items-center justify-center space-y-10 h-[26rem] bg-gradient-to-b from-slate-900 to-black">
 
-        <p className="text-2xl sm:text-3xl font-semibold m-auto text-center">Exchange your tokens across networks.</p>
+        <p className="text-2xl sm:text-3xl font-semibold text-center">Exchange your tokens across networks.</p>
+        <Link href='/app'>
+        <button className="px-4 py-2 bg-blue-500 hover:cursor-pointer font-medium hover:bg-blue-400 rounded-xl">Launch dApp</button>
+        </Link>
       </div>
     </div>
   )
