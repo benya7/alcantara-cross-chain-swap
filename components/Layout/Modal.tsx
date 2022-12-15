906d4cbc1cf67a252d181f489e4ffbc4682494ba
Samsung <bitup.games@gmail.com>
2022-12-15 07:43:38 +0100

    update background color

--- components/Layout/Modal.tsx
+++ components/Layout/Modal.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Modal({ children, onClose }: Props) {
   return (
     <div className="absolute inset-0 flex items-center justify-center z-20">
-      <div className="flex bg-slate-600 h-[28rem] w-96 sm:w-[26rem] rounded-2xl shadow-xl">
+      <div className="flex bg-slate-800 h-[28rem] w-96 sm:w-[26rem] rounded-2xl shadow-xl">
         <div className="relative w-full">
         { onClose && 
             <FiX
