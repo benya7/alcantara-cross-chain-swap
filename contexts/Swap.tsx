35aa07581665c06f80bc9feffbad1d9160c614a2
Samsung <bitup.games@gmail.com>
2022-12-15 11:52:03 +0100

    add text log on insufficientBalance

--- contexts/Swap.tsx
+++ contexts/Swap.tsx
@@ -411,7 +411,9 @@ const SwapProvider = ({ children }: Props) => {
   
   const insufficientBalance = useMemo(() => {
     if (!fromToken || fromTokenAmount === '') return false;
-
+    console.log('fromTokenBalance', fromTokenBalance.toString())
+    console.log('fromTokenAmount', parseUnits(fromTokenAmount, fromToken.decimals).toString())
+    console.log('insufficientBalance', fromTokenBalance.lt(parseUnits(fromTokenAmount, fromToken.decimals)))
     return fromTokenBalance.lt(parseUnits(fromTokenAmount, fromToken.decimals))
 
   },[fromTokenAmount, fromTokenBalance, fromToken])
