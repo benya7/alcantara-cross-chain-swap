d6c107bb3236dd824a2231c3b5c450fdb4b09218
Samsung <bitup.games@gmail.com>
2022-12-16 09:43:36 +0100

    fix calculation tx cost

--- components/TxDetails.tsx
+++ components/TxDetails.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { useSwap } from '../contexts/Swap'
 import { TbGasStation } from "react-icons/tb";
 export default function TxDetails() {
-  const { fullGasCostInUsd } = useSwap()
+  const { txCost } = useSwap()
   return (
     <div className='flex justify-between px-2 items-center'>
       <p className='text-sm inline-flex items-center'><TbGasStation className='mr-1' /> Tx cost:</p>
       {
-        fullGasCostInUsd.state !== 'done' || !(fullGasCostInUsd.value > 0) ?
+        txCost.state === 'fetching' ?
           (
             <div className='bg-slate-500 rounded animate-pulse w-1/4 h-5'></div>
           ) : (
-            <p className='text-sm'>~${fullGasCostInUsd.value.toFixed(2)}</p>
+            <p className='text-sm'>~${txCost.value.toFixed(2)}</p>
           )
       }
     </div>
