711eb5f9609e47ec60f41adbb1f1e81657b04e27
Samsung <bitup.games@gmail.com>
2022-12-16 14:41:10 +0100

    add getExplorerUrl util

---
+++ utils/getExplorerUrl.ts
@@ -0,0 +1,20 @@
+export default function getExplorerUrl(chainId: number) {
+  switch (chainId) {
+    case 1:
+      return "https://etherscan.io";
+    case 137:
+      return "https://polygonscan.io";
+    case 42161:
+      return "https://arbiscan.io";
+    case 250:
+      return "https://ftmscan.io";
+    case 43131:
+      return "https://snowtrace.io";
+    case 56:
+      return "https://bscscan.io";
+    case 1284:
+      return "https://moonscan.io";
+    default:
+      return "https://etherscan.io";
+  }
+}
