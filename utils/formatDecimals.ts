f31d9df4556efbe0157961f9bc1bd458a1a41115
Samsung <bitup.games@gmail.com>
2022-12-17 20:18:15 +0100

    fix conditional error

--- utils/formatDecimals.ts
+++ utils/formatDecimals.ts
@@ -1,5 +1,5 @@
 export default function formatDecimals(value: string) {
-  const slice = value.includes('1') ? value.split('.')[0] : value;
+  const slice = value.includes('.') ? value.split('.')[0] : value;
   let decimalLength = 6;
   return parseFloat(value).toFixed(decimalLength > slice.length ? decimalLength - slice.length : 0);
 }
